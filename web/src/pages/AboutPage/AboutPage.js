faaefb497bbcf5d3ccc02584b82e929ddcb2b8e1
rinesh <rineshgopal@gmail.com>
2020-05-23 13:03:17 +0530

    tutorial blog setup

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+import { Link,routes } from '@redwoodjs/router';
+import BlogLayout from 'src/layouts/BlogLayout';
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <p>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
+      </p>
+      <Link to={routes.home()}>Return home</Link>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
